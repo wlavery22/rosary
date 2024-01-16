ed08bed2d45e58e3e24a28fa81d7576e43c5e58a
William Lavery <lavery_william@yahoo.com>
2024-01-15 21:59:26 -0500

    Add css styling to buttons

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -5,6 +5,7 @@ const ErrorPage = () => {
   return (
     <main className="error-page-container">
       <h1 className="error-message">You have reached this page in error, please go back.</h1>
+      <br></br>
       <Link to="/">
         <button className="return-to-home-btn">Return to Home</button>
       </Link>
